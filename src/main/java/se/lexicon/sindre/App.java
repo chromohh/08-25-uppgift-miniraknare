d9442ac3cc48097b0f5862ecb4cfcc09d1610ee2
chromohh <sindre.he@hotmail.com>
2019-08-23 13:23:30 +0200

    i

--- src/main/java/se/lexicon/sindre/App.java
+++ src/main/java/se/lexicon/sindre/App.java
@@ -1,73 +1,81 @@
 package se.lexicon.sindre;
 
-import com.sun.org.apache.xpath.internal.objects.XString;
-
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App
 {
+    private static Scanner myInput = new Scanner(System.in);
     public static void main( String[] args )
     {
-        Scanner Input = new Scanner(System.in);
         double nummer1;
         double nummer2;
-        System.out.println("***Miniräknare***");
-        String sat;
-        System.out.println("Skriv ditt första tal");
-        nummer1 = Input.nextDouble();
         boolean stop = true;
-        String com;
-        String d;
+        String rmetod;
+
+        System.out.println("***Miniräknare***\nSkriv ditt första tal");
+        nummer1 = convertDouble("s", 1);
+
 
         while(stop){
             System.out.println("Välj räknemetod mellan +, -, /, *");
-            sat = Input.next();
-            System.out.println("skriv ditt andra tal");
-            nummer2 = Input.nextDouble();
+            rmetod = myInput.next();
+            System.out.println("Skriv ditt andra tal");
+            nummer2 = convertDouble("s", 1);
 
-            switch(sat){
+            switch(rmetod){
                 case "+":
                     nummer1 = Add(nummer1, nummer2);
-                    System.out.println("resultat = " +nummer1);
+                    System.out.println("Resultat = " +nummer1);
                     break;
                 case "-":
                     nummer1 = Sub(nummer1, nummer2);
-                    System.out.println("resultat = " +nummer1);
+                    System.out.println("Resultat = " +nummer1);
                     break;
                 case "*" :
                     nummer1 = Mul(nummer1, nummer2);
-                    System.out.println("resultat = " +nummer1);
+                    System.out.println("Resultat = " +nummer1);
                     break;
                 case "/" :
                     nummer1 = Div(nummer1, nummer2);
-                    System.out.println("resultat = " + nummer1);
+                    System.out.println("Resultat = " + nummer1);
                     break;
             }
 
-            System.out.println("1 för att avsluta, valfri tangent för att fortsätta.");
-            d = Input.next();
+            System.out.println("1 för att avsluta \n2 för att nollställa \nValfri tangent för att fortsätta.");
+            String d;
+            d = myInput.next();
             switch(d){
                 case "1":
                     stop = false;
                     break;
+                case "2":
+                    System.out.println("Skriv ditt första tal");
+                    nummer1 = convertDouble("a", 1);;
+                    break;
             }
-
-
 
         }
     }
 
-    public static double Sub(double a, double b){
-        return a - b;
-    }
-    public static double Div(double a, double b){
-        return a / b;
-    }
-    public static double Add(double a, double b){
-        return a + b;
-    }
-    public static double Mul(double a, double b){
-        return a * b;
+    public static double Sub(double a, double b){ return a - b;}
+    public static double Div(double a, double b){ return a / b;}
+    public static double Add(double a, double b){ return a + b;}
+    public static double Mul(double a, double b){ return a * b;}
+    public static double convertDouble(String a, double output)
+    {
+        boolean loop = true;
+        while(loop){
+            try{
+                a = myInput.next();
+                output = Double.parseDouble(a);
+                loop = false;
+            }catch(NumberFormatException e){
+                System.out.println("Fel, skriv ett nummer");
+                loop = true;
+            }
+        }
+        return output;
     }
 
 }
--- src/test/java/se/lexicon/sindre/AppTest.java
+++ src/test/java/se/lexicon/sindre/AppTest.java
@@ -1,20 +1,45 @@
 package se.lexicon.sindre;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    //arrange, sätta upp tester
+    //Act, anropa metoden som ska testas
+    //Assert. Junit testar om resultatet av metodanropet, public void inte statisk.
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void test_Sub(){
+        //arrange
+        double source = 4;
+        double source2 = 2;
+        double expected = 2;
+        //act
+        double actual = App.Sub(source, source2);
+        //assert
+        Assert.assertEquals(expected, actual, 0);
+    }
+    @Test
+    public void test_Add(){
+        double source = 1;
+        double source2 = 2;
+        double expected = 3;
+        double actual = App.Add(source, source2);
+        Assert.assertEquals(expected, actual, 0);
+    }
+    @Test
+    public void test_Div(){
+        double source = 5;
+        double source1 = 2;
+        double expected = 2.5;
+        double actual = App.Div(source, source1);
+        Assert.assertEquals(expected, actual, 0);
+    }
+    @Test
+    public void test_Mul(){
+        double source = 5;
+        double source1 = 5;
+        double expected = 25;
+        double actual = App.Mul(source, source1);
+        Assert.assertEquals(expected, actual, 0);
     }
 }
